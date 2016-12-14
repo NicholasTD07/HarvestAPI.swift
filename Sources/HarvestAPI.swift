46c253a29b15ffd7d5a661ad65782c1bf9ef96fd
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-12-14 22:00:45 +1100

    Add Project and Task with some data spec from HarvestSeason
    https://github.com/NicholasTD07/TTTTT/blob/master/2016-08---Py---Harvest-Season/harvest_season.py

--- Sources/HarvestAPI.swift
+++ Sources/HarvestAPI.swift
@@ -1,25 +1,45 @@
-/**
- *  HarvestAPI
- *
- *  Copyright (c) 2016 Nicholas T.. Licensed under the MIT license, as follows:
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
+public enum Models {
+    public struct Project {
+        public let id: Int
+        public let name: String
+        public let billable: Bool
+        public let tasks: [Task]
+    }
 
-import Foundation
+    public struct Task {
+        public let id: Int
+        public let name: String
+        public let billable: Bool
+    }
+}
+
+    /** From: https://github.com/NicholasTD07/TTTTT/blob/master/2016-08---Py---Harvest-Season/harvest_season.py
+    entry_payload = {
+        'notes': notes,
+        'hours': hours,
+        'project_id': project.id,
+        'task_id': task.id,
+        'spent_at': spent_at,
+    }
+
+    class Project(JSONDeserilizable):
+        __desired_keys__ = ['name', 'id', 'billable']
+        tasks = None
+
+    class Task(JSONDeserilizable):
+        __desired_keys__ = ['name', 'id', 'billable']
+
+    class Entry(JSONDeserilizable):
+        __desired_keys__ = [
+            'id',
+            'hours',
+            'notes',
+            'client',
+            'spent_at', # u'spent_at': u'2016-08-10',
+
+            'project',
+            'project_id',
+            'task',
+            'task_id',
+        ]
+    */
